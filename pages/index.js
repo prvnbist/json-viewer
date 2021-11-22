8e34a1f3efb2649f1c8d55276b2ffe700662d98f
Praveen Bisht <prvnbist@gmail.com>
2021-11-22 10:32:02 +0530

    feat: loading screens

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,7 @@ const findType = (value) => {
 };
 
 export default function IndexPage() {
+  const [status, setStatus] = React.useState('LOADING');
   const [json, setJson] = React.useState({
     string: 'this is a string',
     number: 123,
@@ -41,14 +42,19 @@ export default function IndexPage() {
     setJson(JSON.parse(value));
   }
 
-  function handleEditorDidMount(editor, monaco) {}
+  function handleEditorDidMount(editor, monaco) {
+    setStatus('LOADED');
+  }
 
-  function handleEditorWillMount(monaco) {}
+  function handleEditorWillMount(monaco) {
+    setStatus('LOADING');
+  }
 
   function handleEditorValidation(markers) {
     // model markers
     // markers.forEach(marker => console.log('onValidate:', marker.message));
   }
+
   return (
     <div className="h-full gap-5 grid grid-cols-1 grid-rows-2 lg:grid-rows-1 lg:grid-cols-2">
       <section className="h-full overflow-hidden rounded-lg">
@@ -56,6 +62,7 @@ export default function IndexPage() {
           height="100%"
           width="100%"
           defaultValue="{}"
+          loading={<Loader />}
           defaultLanguage="json"
           onChange={handleEditorChange}
           onMount={handleEditorDidMount}
@@ -64,12 +71,18 @@ export default function IndexPage() {
           value={JSON.stringify(json, null, 3)}
         />
       </section>
-      <JSONViewer json={json} />
+      <JSONViewer json={json} status={status} />
     </div>
   );
 }
 
-const JSONViewer = ({ json = {} }) => {
+const JSONViewer = ({ json = {}, status }) => {
+  if (status === 'LOADING')
+    return (
+      <div className="h-full flex items-center justify-center">
+        <Loader />
+      </div>
+    );
   return (
     <div className="h-full overflow-y-auto space-y-1">
       {_.map(json, (value, key) => (
@@ -165,5 +178,53 @@ const Row = (props) => {
         </aside>
       )}
     </div>
+  );
+};
+
+const Loader = () => {
+  return (
+    <svg
+      width="38"
+      height="38"
+      viewBox="0 0 38 38"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <defs>
+        <linearGradient x1="8.042%" y1="0%" x2="65.682%" y2="23.865%" id="a">
+          <stop stop-color="gray" stop-opacity="0" offset="0%" />
+          <stop stop-color="gray" stop-opacity=".631" offset="63.146%" />
+          <stop stop-color="gray" offset="100%" />
+        </linearGradient>
+      </defs>
+      <g fill="none" fill-rule="evenodd">
+        <g transform="translate(1 1)">
+          <path
+            d="M36 18c0-9.94-8.06-18-18-18"
+            id="Oval-2"
+            stroke="url(#a)"
+            stroke-width="2"
+          >
+            <animateTransform
+              attributeName="transform"
+              type="rotate"
+              from="0 18 18"
+              to="360 18 18"
+              dur="0.9s"
+              repeatCount="indefinite"
+            />
+          </path>
+          <circle fill="#fff" cx="36" cy="18" r="1">
+            <animateTransform
+              attributeName="transform"
+              type="rotate"
+              from="0 18 18"
+              to="360 18 18"
+              dur="0.9s"
+              repeatCount="indefinite"
+            />
+          </circle>
+        </g>
+      </g>
+    </svg>
   );
 };
