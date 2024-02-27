c9940edb84803fefbc7c656f5cd711634667a371
Praveen Bisht <prvnbist@gmail.com>
2024-02-27 10:58:12 +0530

    feat: table view

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import { ColorSchemeScript, MantineProvider } from '@mantine/core'
 
 import '@mantine/core/styles.css'
+import '../styles/global.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
--- src/app/sections/Viewer/index.tsx
+++ src/app/sections/Viewer/index.tsx
@@ -1,10 +1,13 @@
+'use client'
+
 import { useMemo, useState } from 'react'
 import toJsonSchema from 'to-json-schema'
 
-import { Tabs } from '@mantine/core'
+import { Box, ScrollArea, Tabs } from '@mantine/core'
 import { Editor } from '@monaco-editor/react'
 
 import useGlobalStore from '@/state'
+import DataTable from './Table'
 
 const EDITOR_OPTIONS = {
    readonly: true,
@@ -27,33 +30,43 @@ const tabStyles = {
 }
 
 const VIEWS = {
-   SCHEMA: (value: string) => (
+   SCHEMA: (value: Record<any, any>) => (
       <Editor
          theme="vs-dark"
-         value={value}
          onChange={() => {}}
          defaultLanguage="json"
          options={EDITOR_OPTIONS}
+         value={JSON.stringify(value, null, 3)}
       />
+   ),
+   TABLE: (value: Record<any, any>, schema: Record<any, any>) => (
+      <Box p={16}>
+         <ScrollArea h={'calc(100vh - 120px)'} offsetScrollbars>
+            <DataTable value={value} schema={schema} />
+         </ScrollArea>
+      </Box>
    ),
 }
 
 const Viewer = () => {
-   const [tab, setTab] = useState<keyof typeof VIEWS>('SCHEMA')
+   const [tab, setTab] = useState<keyof typeof VIEWS>('TABLE')
 
    const content = useGlobalStore(state => state.content)
 
-   const schema = useMemo(() => {
+   const json = useMemo(() => {
       if (content) {
          try {
-            const _content = toJsonSchema(JSON.parse(content), { arrays: { mode: 'tuple' } })
-            return JSON.stringify(_content, null, 3)
+            return JSON.parse(content)
          } catch (error) {
-            return ''
+            return {}
          }
       }
-      return ''
+      return {}
    }, [content])
+
+   const schema = useMemo(() => {
+      return toJsonSchema(json, { arrays: { mode: 'first' } })
+   }, [json])
 
    return (
       <main>
@@ -61,14 +74,17 @@ const Viewer = () => {
             <Tabs
                value={tab}
                styles={tabStyles}
-               defaultValue="SCHEMA"
                onChange={value => setTab(value as keyof typeof VIEWS)}>
                <Tabs.List>
                   <Tabs.Tab value="SCHEMA">Schema</Tabs.Tab>
+                  <Tabs.Tab value="TABLE">Table</Tabs.Tab>
                </Tabs.List>
             </Tabs>
          </header>
-         <main>{tab === 'SCHEMA' && VIEWS[tab](schema)}</main>
+         <main>
+            {tab === 'SCHEMA' && VIEWS[tab](schema)}
+            {tab === 'TABLE' && VIEWS[tab](json, schema)}
+         </main>
       </main>
    )
 }
